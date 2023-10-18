b69bdbd6ea58ae73d00791a89c8f801065ce395d
Gideon <48917820+GideonIsBuilding@users.noreply.github.com>
2023-10-18 23:42:48 +0100

    Removed uneccessary comments

--- provision-slave.sh
+++ provision-slave.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# --------------------------------------------------------
-# Set the root password
-# --------------------------------------------------------
-# MYSQL_ROOT_PASSWORD="rootDBPass#12"
-
-
 echo "Provisioning slave..."
 
 
@@ -33,15 +27,6 @@ sudo apt install net-tools
 # Install the SSH server
 # --------------------------------------------------------
 sudo apt-get install -y openssh-server
-
-
-# Get IP address of the Master
-# master_ip=$(ifconfig | grep inet | awk '$1=="inet" {print $2}' | head -1)
-
-# --------------------------------------------------------
-# Copy the contents of /mnt/altschool on the master node to /mnt/altschool/slave on the slave node
-# --------------------------------------------------------
-# ssh altschool@192.168.56.5 "mkdir -p /mnt/altschool/slave"
 
 
 # --------------------------------------------------------
@@ -94,44 +79,18 @@ sudo systemctl restart sshd
 sudo apt install -y mysql-server
 
 
-# Initialize MySQL with a default user and password on both nodes
-# ssh altschool@192.168.56.3 "mysql -u root -p < /vagrant/init.sql"
-
-
-# Update the MySQL configuration file
-# sudo sed -i 's/validate_password_policy=0/validate_password_policy=1/g' /etc/mysql/my.cnf
-# sudo sed -i 's/bind-address=127.0.0.1/bind-address=0.0.0.0/g' /etc/mysql/my.cnf
-# sudo sed -i 's/skip-networking/\#skip-networking/g' /etc/mysql/my.cnf
-
-
 # --------------------------------------------------------
-# Start the MySQL Server and grep temporary password
+# Start the MySQL Server and redirect standard error ouput to file
 # --------------------------------------------------------
 echo "Starting MySQL server for the first time"
 
 sudo systemctl start mysql 2> /dev/null
 
-# tempRootPass="`sudo grep 'temporary.*root@localhost' /var/log/mysqld.log | tail -m 1 | sed 's/.*root@localhost: //'`"
-
 
 # --------------------------------------------------------
-# Set new password for root user
+# Secure MySQL installation with the options set in the response.txt file
 # --------------------------------------------------------
-echo "Setting up new mysql server root password"
-# sudo mysql -u "root" --password="$tempRootPass" --connect-expired-password -e "alter user root@localhost identified by '${MYSQL_ROOT_PASSWORD}'; flush privileges;"
-
-
-# --------------------------------------------------------
-# Do the basic hardening
-# --------------------------------------------------------
-# sudo mysql -u root --password="$MYSQL_ROOT_PASSWORD" -e "DELETE FROM mysql.user WHERE User=''; DROP DATABASE IF EXISTS test; DELETE FROM mysql.db WHERE Db='test OR Db='test\\_%'; FLUSH PRIVILEGES;"
-# sudo systemctl status mysql
-
-# --------------------------------------------------------
-# Run the MySQL security script
-# --------------------------------------------------------
-sudo su
-mysql_secure_installation < response.txt
+sudo mysql_secure_installation < response.txt
 
 
 # --------------------------------------------------------
@@ -178,43 +137,6 @@ sudo systemctl reload apache2
 # Display a message indicating the LAMP stack is installed
 # --------------------------------------------------------
 echo "LAMP stack (Apache, MySQL, PHP) has been successfully installed."
-
-
-# --------------------------------------------------------
-# Begin the ssh procedures
-# --------------------------------------------------------
-#./ssh.sh
-
-
-# --------------------------------------------------------
-# Remote machine SSH settings
-# --------------------------------------------------------
-# REMOTE_HOST="192.168.56.5"
-# REMOTE_USER="192.168.56.6"
-
-
-# --------------------------------------------------------
-# SSH key settings
-# --------------------------------------------------------
-# KEY_TYPE="rsa"
-# KEY_FILE="/home/vagrant/.ssh/id_rsa"
-# KEY_COMMENT=""
-
-
-# --------------------------------------------------------
-# Generate SSH key pair
-# --------------------------------------------------------
-# ssh-keygen -t "$KEY_TYPE" -f "$KEY_FILE" -C "$KEY_COMMENT"
-
-
-# --------------------------------------------------------
-# Copy the public key to the remote machine
-# --------------------------------------------------------
-# cat "$KEY_FILE.pub" | ssh "$REMOTE_USER@$REMOTE_HOST" "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
-
-
-# echo "SSH key pair generated and copied to $REMOTE_HOST for $REMOTE_USER."
-
 
 
 # --------------------------------------------------------
